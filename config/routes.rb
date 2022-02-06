e98b6d78a299641e8ab7615ccb97f48f11f60067
Miyauchi <hy.miyauchi@gmail.com>
2022-02-06 17:55:08 +0900

    ユーザー登画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  # users
+  resources :users , only: [:new,:create,:show]
+
+  # sessions
+  resources :sessions, only: [:new,:create,:destroy]
+
+  # pictures
   resources :pictures do
     collection do
       post 'confirm'
